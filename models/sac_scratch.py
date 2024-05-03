e2f12c2b6ceff2dc943e05f70776f6925498a69a
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2024-05-02 22:33:40 -0400

    intermediate changes antv4 implementation

--- models/sac_scratch.py
+++ models/sac_scratch.py
@@ -2,10 +2,47 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.distributions.normal import Normal
+import numpy as np
 
-class Critic(nn.Module):
+class ReplayBuffer():
+    def __init__(self, max_size, input_shape, n_actions):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
+        self.action_memory = np.zeros((self.mem_size, n_actions))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool_)
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+
+        self.mem_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        states_ = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, actions, rewards, states_, dones
+
+
+class CriticNetwork(nn.Module):
     def __init__(self, learning_rate,state_size,action_size, hidden_layer_size):
-        super(Critic).__init__()
+        super(CriticNetwork).__init__()
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.critic = nn.Sequential(
             nn.Linear(state_size + action_size, hidden_layer_size),
@@ -21,9 +58,9 @@ class Critic(nn.Module):
         input = torch.cat([state, action], dim=1)
         return self.critic(input)
 
-class Value(nn.Module):
+class ValueNetwork(nn.Module):
     def __init__(self, learning_rate,state_size, hidden_layer_size):
-        super(Value).__init__()
+        super(ValueNetwork).__init__()
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.value = nn.Sequential(
             nn.Linear(state_size, hidden_layer_size),
@@ -40,14 +77,42 @@ class Value(nn.Module):
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, learning_rate,state_size, action_size, hidden_layer_size):
-        super(Critic).__init__()
+    def __init__(self, learning_rate,state_size, action_size, hidden_layer_size, max_action):
+        super(ActorNetwork).__init__()
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        print(state_size)
+        print(action_size)
         self.actor = nn.Sequential(
             nn.Linear(state_size + action_size, hidden_layer_size),
             nn.ReLU(),
             nn.Linear(hidden_layer_size, hidden_layer_size),
             nn.ReLU(),
         )
+        self.mu = nn.Linear(hidden_layer_size, 1)
+        self.sigma = nn.Linear(hidden_layer_size, 1)
         self.optimizer = optim.Adam(self.parameters, lr= learning_rate)
         self.to(self.device)
+        self.reparam_noise = 1e-6
+        self.max_action = max_action
+
+    def forward(self, state):
+        prob = self.actor(state)
+        mu = self.mu(prob)
+        sigma = self.sigma(prob)
+        sigma = torch.clamp(min=self.reparam_noise, max=1)
+        return mu, sigma
+
+    def sample_normal(self, state, reparam=True):
+        mu, sigma = self.forward(state)
+        probs = Normal(mu, sigma)
+        if reparam:
+            actions = probs.rsample()
+        else:
+            actions = probs.sample()
+        action = (torch.tanh(actions) * self.max_action).to(self.device)
+
+        log_probs = probs.log_prob(actions)
+        log_probs -= torch.log(1-action.pow(2)+self.reparam_noise)
+        log_probs = log_probs.sum(1, keepdim=True)
+
+        return action, log_probs
---
+++ models/sac_torch.py
@@ -0,0 +1,134 @@
+import os
+import torch as T
+import torch.nn.functional as F
+import numpy as np
+from sac_scratch import ActorNetwork, CriticNetwork, ValueNetwork, ReplayBuffer
+import gym
+
+class Agent():
+    def __init__(self, alpha=0.0003, beta=0.0003, input_dims=[8],
+            env=None, gamma=0.99, n_actions=2, max_size=1000000, tau=0.005,
+            layer1_size=256, layer2_size=256, batch_size=256, reward_scale=2):
+        self.gamma = gamma
+        self.tau = tau
+        self.memory = ReplayBuffer(max_size, input_dims, n_actions)
+        self.batch_size = batch_size
+        self.n_actions = n_actions
+        hidden_layer_size = 256
+        self.actor = ActorNetwork(alpha ,input_dims, n_actions, hidden_layer_size, env.action_space.high)
+        self.critic_1 = CriticNetwork(beta, input_dims, n_actions, hidden_layer_size)
+        self.critic_2 = CriticNetwork(beta, input_dims, n_actions, hidden_layer_size)
+
+        self.value = ValueNetwork(beta, input_dims, hidden_layer_size)
+        self.target_value =  ValueNetwork(beta, input_dims, hidden_layer_size)
+
+        self.scale = reward_scale
+        self.update_network_parameters(tau=1)
+
+    def choose_action(self, observation):
+        state = T.Tensor([observation]).to(self.actor.device)
+        actions, _ = self.actor.sample_normal(state, reparameterize=False)
+
+        return actions.cpu().detach().numpy()[0]
+    
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+
+    def update_network_parameters(self, tau=None):
+        if tau is None:
+            tau = self.tau
+
+        target_value_params = self.target_value.named_parameters()
+        value_params = self.value.named_parameters()
+
+        target_value_state_dict = dict(target_value_params)
+        value_state_dict = dict(value_params)
+
+        for name in value_state_dict:
+            value_state_dict[name] = tau*value_state_dict[name].clone() + \
+                    (1-tau)*target_value_state_dict[name].clone()
+
+        self.target_value.load_state_dict(value_state_dict)
+
+
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+
+        state, action, reward, new_state, done = \
+                self.memory.sample_buffer(self.batch_size)
+
+        reward = T.tensor(reward, dtype=T.float).to(self.actor.device)
+        done = T.tensor(done).to(self.actor.device)
+        state_ = T.tensor(new_state, dtype=T.float).to(self.actor.device)
+        state = T.tensor(state, dtype=T.float).to(self.actor.device)
+        action = T.tensor(action, dtype=T.float).to(self.actor.device)
+
+        value = self.value(state).view(-1)
+        value_ = self.target_value(state_).view(-1)
+        value_[done] = 0.0
+
+        actions, log_probs = self.actor.sample_normal(state, reparameterize=False)
+        log_probs = log_probs.view(-1)
+        q1_new_policy = self.critic_1.forward(state, actions)
+        q2_new_policy = self.critic_2.forward(state, actions)
+        critic_value = T.min(q1_new_policy, q2_new_policy)
+        critic_value = critic_value.view(-1)
+
+        self.value.optimizer.zero_grad()
+        value_target = critic_value - log_probs
+        value_loss = 0.5 * F.mse_loss(value, value_target)
+        value_loss.backward(retain_graph=True)
+        self.value.optimizer.step()
+
+        actions, log_probs = self.actor.sample_normal(state, reparameterize=True)
+        log_probs = log_probs.view(-1)
+        q1_new_policy = self.critic_1.forward(state, actions)
+        q2_new_policy = self.critic_2.forward(state, actions)
+        critic_value = T.min(q1_new_policy, q2_new_policy)
+        critic_value = critic_value.view(-1)
+        
+        actor_loss = log_probs - critic_value
+        actor_loss = T.mean(actor_loss)
+        self.actor.optimizer.zero_grad()
+        actor_loss.backward(retain_graph=True)
+        self.actor.optimizer.step()
+
+        self.critic_1.optimizer.zero_grad()
+        self.critic_2.optimizer.zero_grad()
+        q_hat = self.scale*reward + self.gamma*value_
+        q1_old_policy = self.critic_1.forward(state, action).view(-1)
+        q2_old_policy = self.critic_2.forward(state, action).view(-1)
+        critic_1_loss = 0.5 * F.mse_loss(q1_old_policy, q_hat)
+        critic_2_loss = 0.5 * F.mse_loss(q2_old_policy, q_hat)
+
+        critic_loss = critic_1_loss + critic_2_loss
+        critic_loss.backward()
+        self.critic_1.optimizer.step()
+        self.critic_2.optimizer.step()
+
+        self.update_network_parameters()
+
+env = gym.make('Ant-v4')
+n_games = 150
+
+agent = Agent(input_dims=env.observation_space.shape[0], env=env,
+        n_actions=env.action_space.shape[0])
+score_history = []
+for i in range(n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            print(observation)
+            action = agent.choose_action(observation)
+            observation_, reward, done, info, _ = env.step(action)
+            score += reward
+            agent.remember(observation, action, reward, observation_, done)
+            agent.learn()
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
