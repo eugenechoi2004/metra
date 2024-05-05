427fe36ecee5a6be81332618ccbb3bb8304ebd74
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2024-05-05 17:02:49 -0400

    quad

--- envs/dm_env_test.py
+++ envs/dm_env_test.py
@@ -12,20 +12,16 @@ fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = 30
 video_writer = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
 timestep = env.reset()
-frame_count = 0
 
-while not timestep.last() and frame_count < 300:  
+while not timestep.last():  
     action_spec = env.action_spec()
-
     action = np.random.uniform(action_spec.minimum, action_spec.maximum, action_spec.shape)
     timestep = env.step(action)
     print(timestep)
     pixels = env.physics.render(camera_id=camera_id, width=width, height=height, overlays=())
     print(pixels)
-
     bgr_pixels = cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR)
     video_writer.write(bgr_pixels)
-    frame_count += 1
 
 video_writer.release()
 print(f'Video saved as {video_path}')
