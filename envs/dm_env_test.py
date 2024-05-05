a96744e0660739b75a8ebf7a95385c559ea49e9f
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2024-05-05 17:01:20 -0400

    finished the enviorenments

---
+++ envs/dm_env_test.py
@@ -0,0 +1,31 @@
+from dm_control import suite
+import numpy as np
+import cv2
+
+env = suite.load(domain_name='quadruped', task_name='walk')
+
+camera_id = 0
+width, height = 640, 480
+video_path = 'quad_walk.mp4'
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
+fps = 30
+video_writer = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
+timestep = env.reset()
+frame_count = 0
+
+while not timestep.last() and frame_count < 300:  
+    action_spec = env.action_spec()
+
+    action = np.random.uniform(action_spec.minimum, action_spec.maximum, action_spec.shape)
+    timestep = env.step(action)
+    print(timestep)
+    pixels = env.physics.render(camera_id=camera_id, width=width, height=height, overlays=())
+    print(pixels)
+
+    bgr_pixels = cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR)
+    video_writer.write(bgr_pixels)
+    frame_count += 1
+
+video_writer.release()
+print(f'Video saved as {video_path}')
